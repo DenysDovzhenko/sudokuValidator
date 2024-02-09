28e50c62eec19477a8a61475eab737d6d55887d3
Denys Dovzhenko <denis.dovzhenko1@gmail.com>
2024-02-09 12:27:34 +0200

    Code safety and performance is improved

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # sudokuValidator
-A simple sudoku validator written in JS. This is a function that takes a Sudoku representation as a 2d array
+A simple sudoku validator written in JS. This is a function that takes a Sudoku representation as a 2d array.
+Function returns false if array isn`t valid.
+This function is also helpful for arrays with same block size (3x3 block, 4x4 block, 5x5 block, etc.)
+
+validArray - const value. Representation of sorted valid data.
+validArraySum - value of validArray elements sum.
+blockSize - gets size of square block.
+
+getBlock() - gets 2D array representation of the block. Params: sRow - index of start row position; sCol - index of start col position.
+isWrongArraySum() -  soft check is array wrong or not, using only sum equality to correct answer. Example: 9x9 table in one row, col or block should have 45 sum of elements 1+2+3+4+5+6+7+8+9 = 45.
+isWrongArray() - hard check is array (row, col, block) wrong or not. Strong comparison with ideal sorted array.
+
+NOTES
+* It is not very efficient to use sorting algo here, so I will replace it later.
+* I tried to do this code as understandable as useful, but I haven't implemented realization of boards like 12x12 sudoku board (3x4 or 4x3 block). To my mind, i should get as minumum one more param in validSolution() func to define block bigger side
--- sudokuChecker.js
+++ sudokuChecker.js
@@ -1,46 +1,80 @@
 let validSolution = (array) => {
-    const validArray = Array.from({length: array.length}, (_, index) => index + 1); 
-        
-    let blockLength = Math.sqrt(array.length)
-    let getBlock = (sRow, sCol) => array.slice(sRow, sRow + blockLength).map(x => x.slice(sCol, sCol + blockLength))
-   
-    let isWrongArray = (subArray) => {
-        let subarraySorted = subArray.slice().sort()
-        return !validArray.every((val, index) => val === subarraySorted[index])
+    if 
+    (!(
+        array instanceof Array && 
+        array.length !== 0 &&
+        array[0] instanceof Array &&
+        array[0].length !== 0 &&
+        array.length === array[0].length &&
+        array.every(elem => elem.every(x => typeof x === 'number'))
+    )) 
+    {
+        throw new Error("It isn`t 2D square array of integers", { cause: array })
     }
 
-    for (let i = 0; i < validArray.length; i++) {
-        let row = array[i]
-        let col = array.map(x => x[i])           
+    if (!array.every(elem => elem.every(x => x <= array.length))) {
+        throw new Error("Value of sudoku cell cannot be greater than " + array.length)
+    }
 
-        if (isWrongArray(row) && isWrongArray(col)) {
-            return false
-        }
+    if (!array.every(elem => elem.every(x => x >= 0))) {
+        throw new Error("Value of sudoku cell cannot be less than 0")
+    }
 
-        if (i % blockLength === 0) {
-            for (let j = 0; j < validArray.length; j += blockLength) {
-                let block = getBlock(i, j)
-                if (isWrongArray(block.flat())) {
-                    return false
+    const blockSize = Math.sqrt(array.length)
+    let validArraySum = 0
+    const validArray = Array.from({length: array.length}, (_, index) => { 
+        validArraySum += index + 1
+        return index + 1
+    })
+    
+    let getBlock = (sRow, sCol) => array.slice(sRow, sRow + blockSize).map(x => x.slice(sCol, sCol + blockSize))
+    
+    let isWrongArraySum = (subArray) => {
+        let sum = subArray.reduce((accum, curValue) => accum + curValue, 0)
+        return sum !== validArraySum
+    }
+
+    let isWrongArray = (subArray) => {
+        let subArraySorted = subArray.slice().sort()
+        return !validArray.every((val, index) => val === subArraySorted[index])
+    }
+
+    let boardCheck = (checkFunc) => {
+        for (let i = 0; i < validArray.length; i++) {
+            let row = array[i]
+            let col = array.map(x => x[i])           
+
+            if (checkFunc(row) || checkFunc(col)) {
+                return false
+            }
+
+            if (i % blockSize === 0) {
+                for (let j = 0; j < validArray.length; j += blockSize) {
+                    let block = getBlock(i, j)
+                    if (checkFunc(block.flat())) {
+                        return false
+                    }
                 }
             }
         }
+
+        return true
     }
 
-    return true
+    return boardCheck(isWrongArraySum) && boardCheck(isWrongArray) 
 }
 
 console.log(validSolution([
-    [5, 3, 4, 6, 7, 8, 9, 1, 2],
-    [6, 7, 2, 1, 9, 5, 3, 4, 8],
-    [1, 9, 8, 3, 4, 2, 5, 6, 7],
-    [8, 5, 9, 7, 6, 1, 4, 2, 3],
-    [4, 2, 6, 8, 5, 3, 7, 9, 1],
-    [7, 1, 3, 9, 2, 4, 8, 5, 6],
-    [9, 6, 1, 5, 3, 7, 2, 8, 4],
-    [2, 8, 7, 4, 1, 9, 6, 3, 5],
-    [3, 4, 5, 2, 8, 6, 1, 7, 9]
-  ]))
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [10, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9]
+]))
 
 console.log(validSolution([
   [5, 3, 4, 6, 7, 8, 9, 1, 2], 
